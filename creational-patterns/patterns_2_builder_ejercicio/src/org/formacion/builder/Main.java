14e4bf647aafb66578186d303e3b7fd8ceca4308
juanesp-uala <juanmanuelespinosa@WKSAR497L.local>
2021-05-23 19:38:36 -0300

    Exercise builder resolved

--- creational-patterns/patterns_2_builder_ejercicio/src/org/formacion/builder/Main.java
+++ creational-patterns/patterns_2_builder_ejercicio/src/org/formacion/builder/Main.java
@@ -3,8 +3,7 @@ package org.formacion.builder;
 public class Main {
 
 	public static void main (String[] args) {
-
-		// este codigo debe compilar
+		//Este código debe compilar
 		/*
 		Persona madre = new Persona.Builder("Maria")
                 .setMunicipio("Selva")
@@ -16,13 +15,21 @@ public class Main {
                 .setMenor(4)
                 .setColegio("Colegio de Selva")
                 .build();
+
+		System.out.println(madre);
+		System.out.println(hijo);
+
 		*/
-		// esta codigo NO debe compilar
+
+		//Este código NO debe compilar
 		/*
 		Persona mal = new Persona.Builder("Luisa")
 				    .setMayor(20)
 				    .setColegio("Colegio de Villa Arriba")
 				    .build();
-        */
+
+		System.out.println(mal);
+		
+		 */
 	}
 }
--- creational-patterns/patterns_2_builder_ejercicio/src/org/formacion/builder/Persona.java
+++ creational-patterns/patterns_2_builder_ejercicio/src/org/formacion/builder/Persona.java
@@ -9,7 +9,7 @@ public class Persona {
 	private String lugarTrabajo;
 	
 	private Persona() {}
-	
+
 	public static class Builder {
 		private Persona persona;
 		
@@ -22,25 +22,54 @@ public class Persona {
 			persona.municipio = municipio;
 			return this;
 		}
-		
-		public Builder setMayor(int edad, String lugarTrabajo) {
+
+		public BuilderMayor setMayor(int edad) {
 			if (edad < 18) throw new IllegalArgumentException("es menor de edad " + edad);
 			persona.edad = edad;
-			persona.lugarTrabajo = lugarTrabajo;
-			persona.colegio = null;
-			return this;
+			return new Persona.BuilderMayor(this.persona);
 		}
-		
-		public Builder setMenor(int edad, String colegio) {
+
+		public BuilderMenor setMenor(int edad) {
 			if (edad >= 18) throw new IllegalArgumentException("es mayor de edad " + edad);
 			persona.edad = edad;
-			persona.colegio = colegio;
-			persona.lugarTrabajo = null;
+			return new Persona.BuilderMenor(this.persona);
+		}
+	}
+
+	public static class BuilderMayor {
+
+		private Persona persona;
+
+		public BuilderMayor(Persona persona){
+			this.persona = persona;
+		}
+
+		public BuilderMayor setLugarTrabajo(String lugarTrabajo){
+			this.persona.lugarTrabajo = lugarTrabajo;
 			return this;
 		}
-	
-		public Persona build() {
-			return persona;
+
+		public Persona build(){
+			return this.persona;
+		}
+
+	}
+
+	public static class BuilderMenor {
+
+		private Persona persona;
+
+		public BuilderMenor(Persona persona){
+			this.persona = persona;
+		}
+
+		public BuilderMenor setColegio(String colegio){
+			this.persona.colegio = colegio;
+			return this;
+		}
+
+		public Persona build(){
+			return this.persona;
 		}
 
 	}
